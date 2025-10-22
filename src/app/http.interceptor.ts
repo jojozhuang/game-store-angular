367edb0b974c9acd3f0736cd6e750e187962beff
Rong Zhuang <jojozhuang@gmail.com>
2025-10-21 22:42:52 -0700

    Migrate .eslintrc.json to eslint.config.mjs (#51)
    * Migrate .eslintrc.json to eslint.config.mjs
    
    * upgrade eslint libs
    
    * fix lint errors

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -4,7 +4,6 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
-  HttpResponse,
   HttpErrorResponse,
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
@@ -14,23 +13,15 @@ import { ResponseResult } from './models';
 
 @Injectable()
 export class ErrorHttpInterceptor implements HttpInterceptor {
-  // eslint-disable-next-line
   intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+    req: HttpRequest<any>, // eslint-disable-line
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> { // eslint-disable-line
     return next.handle(req).pipe(
       tap(
-        (event) => {
-          //console.log(customReq);
-          /*if (ev instanceof HttpResponse) {
-                    //console.error(ev);
-                    //console.log('processing response', ev);
-                }*/
-        },
+        (event) => {}, // eslint-disable-line
         (response) => {
           const respResult = new ResponseResult(200, '');
-          //console.error(response);
           if (response instanceof HttpErrorResponse) {
             const err = response.message || JSON.stringify(response.error);
             respResult.statusCode = response.status;
@@ -39,7 +30,6 @@ export class ErrorHttpInterceptor implements HttpInterceptor {
             respResult.statusCode = 400;
             respResult.message = response.message ? response.message : response.toString(); // eslint-disable-line
           }
-          //console.error(respResult.message);
           return respResult;
         },
       ),

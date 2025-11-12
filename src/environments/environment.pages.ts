492a1c020b7e96cfba2f0a68ccd68089ea050240
jojozhuang <jojozhuang@gmail.com>
2025-11-11 19:47:31 -0800

    use azure api

--- src/environments/environment.pages.ts
+++ src/environments/environment.pages.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   envName: 'GitHub Pages',
-  apiUrl: 'https://game-store-springboot.onrender.com/',
+  apiUrl: 'https://gamestore-api.azurewebsites.net/',
 };
